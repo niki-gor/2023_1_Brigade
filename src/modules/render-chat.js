15dc32fb96d2681120772de64581dd939d86a5bb
Zela2520 <sugarbeatzela@gmail.com>
2023-03-05 13:10:25 +0300

    logout has been added

--- src/modules/render-chat.js
+++ src/modules/render-chat.js
@@ -1,7 +1,15 @@
 import chat from '../templates/chat.js';
+import { post } from './ajax.js';
 
 export default (parent, config) => {
 	parent.innerHTML = '';
-
 	parent.innerHTML = chat();
+
+	document.querySelector(".logout").addEventListener('click', (e) => {
+		e.preventDefault();
+
+		// TODO: POST
+
+		config.auth.render(parent, config);
+	});
 }
