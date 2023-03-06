0ea9b3c0f9fe19f635e73ac5d651f50ec6a51cc7
Zela2520 <sugarbeatzela@gmail.com>
2023-03-06 15:34:38 +0300

    change remote address

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import config from './modules/config.js';
+// import checkAuth from './modules/render-auth.js';
 
 const root = document.querySelector('.root');
+
+
+// if == checkAuth() {
+
+// }
 
 config.login.render(root, config);
