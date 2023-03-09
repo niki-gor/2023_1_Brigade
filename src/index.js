62bec4d802c5ff5afcddbb742586a210d296770d
Zela2520 <sugarbeatzela@gmail.com>
2023-03-09 15:53:32 +0300

    auth styles fixed

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import config from './modules/renders/config.js';
 
 const root = document.querySelector('.root');
 
-config.auth.render(root, config);
-// config.chat.render(root, config);
+// config.auth.render(root, config);
+config.login.render(root, config);
