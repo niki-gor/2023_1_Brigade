757d8dacece2d74aa92971b7195cd3ab761ac73f
Zela2520 <sugarbeatzela@gmail.com>
2023-04-07 16:15:38 +0300

    chat render component done

--- src/containers/chat/chat.ts
+++ src/containers/chat/chat.ts
@@ -1,6 +1,8 @@
 import { Container } from "@containers/container";
 import { store } from "@/store/store";
 import { DumbChat } from "@/components/chat/chat";
+import { createRenderAction } from "@/actions/routeActions";
+import { Message } from "@/components/message/message";
 
 
 export interface SmartChat {
@@ -8,8 +10,6 @@ export interface SmartChat {
         isSubsribed: boolean,
         domElements: {
             submitBtn: HTMLElement | null;
-            searchBtn: HTMLElement | null;
-            chatInfoBtn: HTMLElement | null;
         }
     }
 }
@@ -30,8 +30,6 @@ export class SmartChat extends Container {
             isSubsribed: false,
             domElements: {
                 submitBtn: null,
-                searchBtn: null,
-                chatInfoBtn: null,
             }
         }
     }
@@ -41,8 +39,12 @@ export class SmartChat extends Container {
      */
     render() {
         if (this.state.isSubsribed) {
-            const chat = new DumbChat({ avatar: './assets/img/geva.png', 
-                                        username: 'Gevork Gabrielyan',
+            // TODO: this.state.openChat.avatar и тд
+            const chat = new DumbChat({ avatar: './assets/img/geva.png', // this.props.avatar
+                                        username: 'Gevork Gabrielyan', // this.props.username
+                                        title: this.props.title,
+                                        messages: this.props.messages,
+                                        members: this.props.members,
                                         userStatus: 'ha ha chill',
                                         userOnline: false,
                                         friendAvatar: './assets/img/iii.png',
@@ -51,9 +53,34 @@ export class SmartChat extends Container {
             }); // TODO: прокидываем объект из swagger-a
 
             this.rootNode.innerHTML = chat.render();
+
+            this.state.domElements.submitBtn = document.querySelector('.view-chat__send-message-button');
+            this.state.domElements.submitBtn?.addEventListener('click', (e) => {
+                e.preventDefault();
+                const sendBtn = e.target as HTMLElement;
+
+                this.handleClickSendButton(sendBtn);
+            })
+
         }
     }
 
+    handleClickSendButton(sendBtn: HTMLElement) {
+        // TODO: из input-a забираем текст
+        //  Создаем сообщение,
+        const input = document.querySelector('.input-message__text-field__in') as HTMLInputElement;
+        if (input.value) {
+            const message = new Message({
+                messageContent: input.value,
+                messageAvatar: './assets/img/geva.png', // TODO: this.state.avatar
+                messageSide: true, // true - левая сторона
+                username: 'Gevork Gabrielyan', // TODO: this.state.username
+            })
+
+            // TODO: this.state.messages.push(message); - websocket ???
+            // TODO: store.dispatch(createMessageDialogAction(this.props.messages.push(input))); - хер знает как там в store будет изменяться
+        }
+    }
 
     componentDidMount() {
         if (!this.state.isSubsribed) {
@@ -64,14 +91,10 @@ export class SmartChat extends Container {
             }))
 
             this.state.isSubsribed = true;
+            store.dispatch(createRenderAction());
         }
-
-        // store.dispatch(createRenderAction()); // говорим что че то произошло
     }
 
-    /**
-     * Удаляет все подписки
-     */
     componentWillUnmount() {
         this.unsubscribe.forEach((uns) => uns());
         this.state.isSubsribed = false;
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
 import '@/index.css';
 import { store } from '@store/store';
 import { ROOT } from './config/config';
-// import { createAuthAction } from './actions/authActions';
-// import { SmartSidebar } from '@/containers/sidebar/sidebar';
+import { SmartSidebar } from '@/containers/sidebar/sidebar';
 import { SmartChat } from './containers/chat/chat';
 
 
-
-// document.querySelector('#root')?.innerHTML = new DumbChat({}).render();
+// const sidebar = new SmartSidebar({...store.getState(), rootNode: ROOT})
+// sidebar.componentDidMount();
+// sidebar.render();
 
 const chat = new SmartChat({...store.getState(), rootNode: ROOT});
 chat.componentDidMount();
-// chat.componentWillUnmount();
 chat.render();
-
-// store.dispatch(createAuthAction());
-
-
