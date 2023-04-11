f014e2bea997f5cba674a13fe1772330719b85d7
Zela2520 <sugarbeatzela@gmail.com>
2023-04-12 00:31:04 +0300

    fixed createMoveToChatAction

--- src/containers/chat/chat.ts
+++ src/containers/chat/chat.ts
@@ -42,6 +42,7 @@ export class SmartChat extends Container {
      * Рендерит чат
      */
     render() {
+        console.log(this.chatId);
         if (this.props?.openedChat?.isNotRendered) {
             if (this.state.isSubscribed && this.chatId) {
                 const chat = new DumbChat({
