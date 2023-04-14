e16938bef9b94372315f8404cf5cfdad85deec43
Zela2520 <sugarbeatzela@gmail.com>
2023-04-14 12:22:43 +0300

    started making error page, corrected profile styles

---
+++ src/components/error/error.ts
@@ -0,0 +1,14 @@
+import { Component } from "@components/component";
+import template from "@components/error/error.pug";
+
+export class DumbContact extends Component {
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        return template({
+            
+        });
+    }
+}
