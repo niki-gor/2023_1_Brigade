851c7a4ce9700ea98543c19a42501778859f0874
Gevork Gabrielyan <gevorkgabrielan@gmail.com>
2023-04-29 17:02:53 +0300

    Fixed api for creatin channel and did dropdown adaptive

--- src/components/chatList/chatList.ts
+++ src/components/chatList/chatList.ts
@@ -2,7 +2,6 @@ import { Component } from "@/components/component";
 import template from "@components/chatList/chatList.pug";
 import "@components/chatList/chatList.scss"
 import { DumbChatCard } from "@components/chatCard/chatCard";
-import { svgButtonUI } from "@components/ui/button/button";
 import { searchUi } from "@components/search/search";
 import { Dropdown } from "@components/dropdown/dropdown";
 
@@ -26,7 +25,17 @@ export class DumbChatList extends Component {
     render() {
         return template({
             headChats: 'Чаты',
-            dropdown: new Dropdown({}).render(),
+            dropdown: new Dropdown({
+                icon: "create-btn",
+                list: [{
+                    className: "dropdown-menu__item-group",
+                    value: "Создать группу",
+                },
+                {
+                    className: "dropdown-menu__item-channel",
+                    value: "Создать канал",
+                }],
+            }).render(),
             chatInput: new searchUi({
                 inputClassName: "chats__header__input",
                 placeholder: "Поиск"
