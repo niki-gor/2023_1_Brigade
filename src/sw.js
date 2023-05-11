0431490d686266c6dab195f3c1725dee77605062
Gevork Gabrielyan <gevorkgabrielan@gmail.com>
2023-05-12 01:49:38 +0300

    Fixed sw

--- src/sw.js
+++ src/sw.js
@@ -2,51 +2,27 @@ const CACHE_NAME = 'technogram-cache';
 const urlsToCache = ['/']; //? Добавить ли ../dist
 const cacheWhitelist = []; //? Что у нас можно не обновлять
 
+const imageRegRex = /.webp|.svg|.jpg|.jpeg|.gif|.png/;
+
 self.addEventListener('install', (event) => {
-    //? если файл ts, то ругается что waitUntil нет в Event
+    // self.skipWaiting(); //? хз нахера
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            console.log('Opened cache');
-            return cache.addAll(urlsToCache);
-        })
-    );
-});
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            if (response) {
-                return response;
-            }
-
-            const fetchRequest = event.request.clone();
-
-            return fetch(fetchRequest).then((response) => {
-                if (
-                    !response ||
-                    (response.status !== 200 &&
-                        response.status !== 201 &&
-                        response.status !== 204) ||
-                    response.type !== 'basic'
-                ) {
-                    return response;
-                }
-
-                const responseToCache = response.clone();
-
-                caches.open(CACHE_NAME).then((cache) => {
-                    cache.put(event.request, responseToCache);
-                });
-
-                return response;
-            });
-        })
+        caches
+            .open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+            .catch((err) => {
+                console.error('Failed install sw:', err);
+            })
     );
 });
 
 self.addEventListener('activate', (event) => {
     event.waitUntil(
         caches.keys().then((cacheNames) => {
+            console.log('Activated cache');
             return Promise.all(
                 cacheNames.map((cacheName) => {
                     if (cacheWhitelist.indexOf(cacheName) === -1) {
@@ -56,4 +32,81 @@ self.addEventListener('activate', (event) => {
             );
         })
     );
+    // event.waitUntil(self.clients.claim()); //? хз нахера
 });
+
+self.addEventListener('fetch', (event) => {
+    const { request } = event;
+    if (request.method !== 'GET') {
+        return;
+    }
+
+    if (
+        imageRegRex.test(event.request) &&
+        !event.request.path.includes('avatar')
+    ) {
+        event.respondWith(cacheFirst(event));
+    } else {
+        event.respondWith(networkFirst(event));
+    }
+});
+
+async function networkFirst(event) {
+    const { request } = event;
+    const cache = await caches.open(CACHE_NAME);
+    try {
+        const response = await fetch(request);
+
+        if (navigator.onLine) {
+            await cache.put(request, response.clone());
+        }
+        return response;
+    } catch {
+        try {
+            const cachedResponse = await cache.match(request);
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+        } catch {
+            const response = await event.preloadResponse;
+            if (response) {
+                return response;
+            } else {
+                return new Response(null, {
+                    status: 404,
+                    statusText: 'Not Found',
+                });
+            }
+        }
+    }
+}
+
+async function cacheFirst(event) {
+    const { request } = event;
+    const cache = await caches.open(CACHE_NAME);
+    try {
+        const cachedResponse = await cache.match(request);
+        if (cachedResponse) {
+            return cachedResponse;
+        }
+
+        const response = await event.preloadResponse;
+        if (response) {
+            return response;
+        }
+    } catch {
+        try {
+            const response = await fetch(request);
+
+            if (navigator.onLine) {
+                await cache.put(request, response.clone());
+            }
+            return response;
+        } catch {
+            return new Response(null, {
+                status: 404,
+                statusText: 'Not Found',
+            });
+        }
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -90,6 +90,10 @@ module.exports = {
                     from: path.resolve(__dirname, 'src/sw.js'),
                     to: path.resolve(__dirname, 'dist'),
                 },
+                {
+                    from: path.resolve(__dirname, 'src/offline.html'),
+                    to: path.resolve(__dirname, 'dist'),
+                },
             ],
         }),
         new MiniCssExtractPlugin({
